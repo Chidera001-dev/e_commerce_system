cede05c86c10e5fa19b88a77996fa9da8b6565b5
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-11-14 10:09:50 +0100

    feat: enhance ProductFilter and PublicProductListAPIView with additional filtering options and improved queryset handling

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -255,7 +255,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
-    "DEFAULT_FILTER_BACKENDS": [
+     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
