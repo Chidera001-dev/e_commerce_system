740ed2ce535f0041ea839a4ee764c4e621f8f119
Chidera001-dev <nnamanichideraemmanuel7@gmail.com>
2025-11-29 19:18:05 +0100

    Refactor order and product models to use ShortUUID for primary keys; update migrations for orders, products, and services
    - Removed and added fields in the Order model for better shipping information management.
    - Updated OrderItem and Order models to ensure consistent use of ShortUUID for IDs.
    - Created new migrations to reflect changes in the database schema.
    - Adjusted the UserAdmin to include phone number in the admin interface.
    - Refactored ShippingAddress and Shipment models for improved clarity and functionality.

--- product/migrations/0019_alter_category_id_alter_product_id.py
+++ product/migrations/0019_alter_category_id_alter_product_id.py
@@ -7,17 +7,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('carts', '0003_alter_cart_id_alter_cartitem_id'),
+        ('product', '0018_alter_category_id_alter_product_id'),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='cart',
+            model_name='category',
             name='id',
             field=models.CharField(default=shortuuid.main.ShortUUID.uuid, editable=False, max_length=22, primary_key=True, serialize=False, unique=True),
         ),
         migrations.AlterField(
-            model_name='cartitem',
+            model_name='product',
             name='id',
             field=models.CharField(default=shortuuid.main.ShortUUID.uuid, editable=False, max_length=22, primary_key=True, serialize=False, unique=True),
         ),
